1705160864f59709c1253a9b2c8cf621c68dd5a9
Tony Hung <tbass134@gmail.com>
2022-05-03 17:19:20 -0400

    added datasets

--- app.py
+++ app.py
@@ -5,3 +5,4 @@ base_dir = "../drive/MyDrive/Kaggle Datasets/petfinder-pawpularity-score/"
 train_df = pd.read_csv(base_dir + "train.csv")
 test_df = pd.read_csv(base_dir + "train.csv")
 print(train_df.head())
+
